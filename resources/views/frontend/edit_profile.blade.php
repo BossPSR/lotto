ef712e70e0191587649857d592fc597dece791d4
SanyaluckYeemou <sanyaluck503swr@gmail.com>
2020-08-09 16:17:29 +0700

    ปรับแก้ไข alert ตอนบันทึกเสร็จ

--- resources/views/frontend/edit_profile.blade.php
+++ resources/views/frontend/edit_profile.blade.php
@@ -131,6 +131,7 @@ echo csrf_token() ?>">
 </div>
 <!-- jackpot end -->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="{{url('backend/app-assets/vendors/js/extensions/sweetalert2.all.min.js')}}"></script>
 <script>
     var csrf_token = '{{ csrf_token() }}';
 
@@ -154,6 +155,15 @@ echo csrf_token() ?>">
     $("#imgInp").change(function() {
         readURL(this);
     });
+
+    @if(session()->has('message'))
+    Swal.fire({
+        type: '{{ session()->get("status") }}',
+        title: '<small> {{ session()->get("message") }} </small>',
+        showConfirmButton: false,
+        timer: 5000
+    });
+    @endif
 </script>
 
 @endsection
